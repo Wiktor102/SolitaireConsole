47967fd9fb858ef60d261b8f3855d9c3fa3f0e9b
Wiktor Golicz <wiktorgolicz1@gmail.com>
2025-05-17 09:53:59 +0200

    Generic Menu class. implemented in DifficultySelector

--- UI/DifficultySelector.cs
+++ UI/DifficultySelector.cs
@@ -1,115 +1,23 @@
 using SolitaireConsole.UI;
-using System.Text;
 
 namespace SolitaireConsole {
 	/// <summary>
 	/// Klasa odpowiedzialna za wyświetlanie menu wyboru poziomu trudności
 	/// </summary>
 	public class DifficultySelector {
-		private string[] options = ["Łatwy (dobieranie 1 karty)", "Trudny (dobieranie 3 kart)", "Wyjdź"];
-		private int selectedIndex = 0;
-		private readonly MenuRenderer _renderer = new ConsoleMenuRenderer();
+		private readonly Menu<DifficultyLevel?> _menu = new(Menu<int>.GAME_TITLE_HEADING, [
+				new("Łatwy (dobieranie 1 karty)", DifficultyLevel.Easy),
+				new("Trudny (dobieranie 3 kart)", DifficultyLevel.Hard),
+				new("Wyjdź", null)
+			]
+		);
 
 		/// <summary>
 		/// Wyświetla menu wyboru poziomu trudności i zwraca wybrany poziom
 		/// </summary>
 		/// <returns>DifficultyLevel.Easy, DifficultyLevel.Hard lub null dla wyjścia</returns>
 		public DifficultyLevel? ChooseDifficulty() {
-			ConsoleKeyInfo key;
-
-			do {
-				DisplayMenu();
-				key = Console.ReadKey(true); // true - nie wyświetlaj wciśniętego klawisza
-				HandleKeyInput(key);
-
-				if (key.Key == ConsoleKey.Enter || key.Key == ConsoleKey.Escape) {
-					return ProcessSelection();
-				}
-			} while (true);
-		}
-
-		/// <summary>
-		/// Wyświetla menu wyboru poziomu trudności
-		/// </summary>
-		private void DisplayMenu() {
-			Console.Clear();
-
-			Console.ForegroundColor = ConsoleColor.Green;
-			string[] heading = [
-				@" ______   ______     ______       __     ______     __   __     ______   ",
-				@"/\  == \ /\  __ \   /\  ___\     /\ \   /\  __ \   /\ ""-.\ \   /\  ___\  ",
-				@"\ \  _-/ \ \  __ \  \ \___  \   _\_\ \  \ \  __ \  \ \ \-.  \  \ \___  \ ",
-				@" \ \_\    \ \_\ \_\  \/\_____\ /\_____\  \ \_\ \_\  \ \_\\""\_\  \/\_____\",
-				@"  \/_/     \/_/\/_/   \/_____/ \/_____/   \/_/\/_/   \/_/ \/_/   \/_____/"
-			];
-			_renderer.DisplayText(heading);
-			Console.ResetColor();
-			Console.WriteLine();
-			_renderer.DisplayTextLine("Wybierz poziom trudności:");
-
-			for (int i = 0; i < options.Length; i++) DisplayMenuItem(i);
-			_renderer.DisplayDividerLine();
-		}
-
-		/// <summary>
-		/// Wyświetla pojedynczą opcję menu
-		/// </summary>
-		private void DisplayMenuItem(int index) {
-			_renderer.DisplayMenuOption(options[index], index == selectedIndex);
-		}
-
-		/// <summary>
-		/// Obsługuje naciśnięcie klawiszy (strzałki góra/dół)
-		/// </summary>
-		private void HandleKeyInput(ConsoleKeyInfo key) {
-			switch (key.Key) {
-				case ConsoleKey.UpArrow:
-					UpdateSelectedIndex(-1);
-					break;
-				case ConsoleKey.DownArrow:
-					UpdateSelectedIndex(1);
-					break;
-				case ConsoleKey.Escape: // Dodatkowa opcja wyjścia przez Escape
-					selectedIndex = options.Length - 1; // Ustaw na "Wyjdź"
-					break;
-			}
-		}
-
-		/// <summary>
-		/// Aktualizuje indeks wybranej opcji z odpowiednią obsługą zawijania
-		/// </summary>
-		private void UpdateSelectedIndex(int direction) {
-			if (direction < 0) {
-				// Przesuń w górę (z zawijaniem)
-				selectedIndex = (selectedIndex == 0) ? options.Length - 1 : selectedIndex - 1;
-			} else {
-				// Przesuń w dół (z zawijaniem)
-				selectedIndex = (selectedIndex == options.Length - 1) ? 0 : selectedIndex + 1;
-			}
-		}
-
-		/// <summary>
-		/// Przetwarza wybraną opcję i zwraca odpowiednią wartość
-		/// </summary>
-		private DifficultyLevel? ProcessSelection() {
-			Console.Clear(); // Wyczyść menu przed wyświetleniem komunikatu
-
-			switch (selectedIndex) {
-				case 0:
-					Console.WriteLine("Wybrano poziom łatwy.");
-					Thread.Sleep(500);
-					return DifficultyLevel.Easy;
-				case 1:
-					Console.WriteLine("Wybrano poziom trudny.");
-					Thread.Sleep(500);
-					return DifficultyLevel.Hard;
-				case 2:
-					Console.WriteLine("Wyjście z gry.");
-					Thread.Sleep(500);
-					return null; // Sygnał do wyjścia z gry
-				default:
-					return null;
-			}
+			return _menu.Select();
 		}
 	}
 }
--- UI/Menu.cs
+++ UI/Menu.cs
@@ -1,4 +1,81 @@
 namespace SolitaireConsole.UI {
+	public class MenuOption<T>(string label, T value) {
+		public readonly string Label = label;
+		public readonly T Value = value;
+	}
+
+	public class Menu<T> {
+		public static readonly string[] GAME_TITLE_HEADING = [
+			@" ______   ______     ______       __     ______     __   __     ______   ",
+			@"/\  == \ /\  __ \   /\  ___\     /\ \   /\  __ \   /\ ""-.\ \   /\  ___\  ",
+			@"\ \  _-/ \ \  __ \  \ \___  \   _\_\ \  \ \  __ \  \ \ \-.  \  \ \___  \ ",
+			@" \ \_\    \ \_\ \_\  \/\_____\ /\_____\  \ \_\ \_\  \ \_\\""\_\  \/\_____\",
+			@"  \/_/     \/_/\/_/   \/_____/ \/_____/   \/_/\/_/   \/_/ \/_/   \/_____/"
+		];
+
+		protected string[] heading;
+		protected MenuOption<T>[] options;
+		protected int selectedIndex = 0;
+		private readonly MenuRenderer _renderer = new ConsoleMenuRenderer(); // Kompozycja
+
+		public Menu(string[] heading, MenuOption<T>[] options) {
+			if (options.Length < 1) throw new ArgumentException("Menu must have at least one option.");
+			this.heading = heading;
+			this.options = options;
+		}
+
+		public T Select() {
+			ConsoleKeyInfo key;
+
+			do {
+				Display();
+				key = Console.ReadKey(true); // true - nie wyświetlaj wciśniętego klawisza
+				HandleKeyInput(key);
+
+				if (key.Key == ConsoleKey.Enter) {
+					return options[selectedIndex].Value;
+				}
+			} while (true);
+		}
+
+		private void Display() {
+			Console.Clear();
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			_renderer.DisplayText(heading);
+			Console.ResetColor();
+			Console.WriteLine();
+			_renderer.DisplayTextLine("Wybierz poziom trudności:");
+
+			for (int i = 0; i < options.Length; i++) {
+				_renderer.DisplayMenuOption(options[i].Label, i == selectedIndex);
+			}
+
+			_renderer.DisplayDividerLine();
+		}
+
+		private void HandleKeyInput(ConsoleKeyInfo key) {
+			switch (key.Key) {
+				case ConsoleKey.UpArrow:
+					UpdateSelectedIndex(-1);
+					break;
+				case ConsoleKey.DownArrow:
+					UpdateSelectedIndex(1);
+					break;
+			}
+		}
+
+		private void UpdateSelectedIndex(int direction) {
+			if (direction < 0) {
+				// Przesuń w górę (z zawijaniem)
+				selectedIndex = (selectedIndex == 0) ? options.Length - 1 : selectedIndex - 1;
+			} else {
+				// Przesuń w dół (z zawijaniem)
+				selectedIndex = (selectedIndex == options.Length - 1) ? 0 : selectedIndex + 1;
+			}
+		}
+	}
+
 	public abstract class MenuRenderer {
 		protected const int WIDTH = 83;
 		
